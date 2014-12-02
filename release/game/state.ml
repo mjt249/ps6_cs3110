2e10799dba0c0e956a39ae26c38a0bf713559b4b
sheroze1123 <mss385@cornell.edu>
2014-12-01 21:13:05 -0500

    Expected action

--- release/game/state.ml
+++ release/game/state.ml
@@ -10,6 +10,7 @@ module GameState = struct
   type player = {
     mutable inv : inventory;
     mutable active_steammon : steammon option;
+    mutable expected_action : action;
   }
 
   type state = { 
@@ -21,14 +22,25 @@ module GameState = struct
     mutable blue : player;
   }
 
-  let init_player () = {inv = []; active_steammon = None;}
+  let init_red () = {
+    inv = []; 
+    active_steammon = None;
+    expected_action = SendTeamName "Red";
+  }
+
+  let init_blue () = {
+    inv = []; 
+    active_steammon = None;
+    expected_action = SendTeamName "Blue";
+  }
+
   let initial_state () = {
     red_name = None;
     blue_name = None;
     mvs = [];
     mons = [];
-    red = init_player ();
-    blue = init_player ();
+    red = init_red ();
+    blue = init_blue ();
   }
 
   let get_red_name s = s.red_name
@@ -37,6 +49,8 @@ module GameState = struct
   let get_steammon_list s = s.mons
   let get_red_inv s = s.red.inv
   let get_blue_inv s = s.blue.inv
+  let get_red_exp s = s.red.expected_action
+  let get_blue_exp s = s.blue.expected_action
 
   let set_red_name s name = s.red_name <- (Some name)
   let set_blue_name s name = s.blue_name <- (Some name)
@@ -44,4 +58,6 @@ module GameState = struct
   let set_steammon_list s mon_list = s.mons <- mon_list
   let set_red_inv s inv = s.red.inv <- inv
   let set_blue_inv s inv = s.blue.inv <- inv
+  let set_red_exp s a = s.red.expected_action <- a
+  let set_blue_exp s a = s.blue.expected_action <- a
 end
