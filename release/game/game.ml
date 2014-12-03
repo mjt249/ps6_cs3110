e7f55ce23cb4f286336dccb616e1d8f85660fe46
sheroze1123 <mss385@cornell.edu>
2014-12-02 23:45:19 -0500

    Started battle phase

--- release/game/game.ml
+++ release/game/game.ml
@@ -14,11 +14,11 @@ type game = GameState.state
 let game_instance = ref (GameState.initial_state ()) 
 
 let game_datafication (g:game) : game_status_data =
-  let r_mons = GameState.get_player_steammons g Red in
+  let r_mons = GameState.get_steammon_list g Red in
   let r_inv = GameState.get_inv g Red in
   let r_creds = GameState.get_creds g Red in
   let r_team = (r_mons, r_inv, r_creds) in
-  let b_mons = GameState.get_player_steammons g Blue in
+  let b_mons = GameState.get_steammon_list g Blue in
   let b_inv = GameState.get_inv g Blue in
   let b_creds = GameState.get_creds g Blue in
   let b_team = (b_mons, b_inv, b_creds) in
@@ -28,17 +28,70 @@ let game_from_data (game_data:game_status_data) : game =
   let (r_team, b_team) = game_data in
   let (r_mons, r_inv, r_creds) = r_team in
   let (b_mons, b_inv, b_creds) = b_team in
-  GameState.set_player_steammons !game_instance Red r_mons;
+  (*GameState.set_steammons !game_instance Red r_mons;*)
   GameState.set_inv !game_instance Red r_inv;
   GameState.set_creds !game_instance Red r_creds;
-  GameState.set_player_steammons !game_instance Blue b_mons;
+  (*GameState.set_steammons !game_instance Blue b_mons;*)
   GameState.set_inv !game_instance Blue b_inv;
   GameState.set_creds !game_instance Blue b_creds;
   !game_instance
 
 let draft_phase g ra ba = failwith "Implement draft_phase"
 let stocking_inventory g ra ba = failwith "Implement inventory phase"
-let battle_phase g ra ba = failwith "Implement battle_phase"
+
+(* steammon if there exists at least one steammon that has not fainted,
+ * None otherwise *)
+let rec faint_check (lst:steammon list) : steammon option =
+  match lst with
+  | [] -> failwith "No steammons in list"
+  | h::[] when h.curr_hp > 0 -> Some h
+  | h::[] -> None
+  | h::t when h.curr_hp > 0 -> Some h
+  | h::t -> faint_check t
+
+let opp_color c =
+  match c with
+  | Red -> Blue
+  | Blue -> Red 
+
+(*Tries to get an arbitrary_starter steammon, if all steammons have*)
+(*fainted, check whether all the opposing steammons have fainted.*)
+(*If so, game is a tie. If not, the opposing team wins. If there is*)
+(*a steammon on the team that has not fainted, return that steammon.*)
+let arbitrary_starter g c : steammon option * game_result option = 
+  let steammon_list = GameState.get_steammon_list g c in
+  match (faint_check steammon_list) with
+  | None -> 
+      (let opp_steammon_list = GameState.get_steammon_list g (opp_color c) in
+      match (faint_check opp_steammon_list) with
+      | None -> (None, Some Tie)
+      | _ -> (None, Some (Winner (opp_color c))))
+  | Some s -> (Some s, None)
+
+let valid_steammon s reserve_pool : bool = 
+  (Table.mem reserve_pool s) && 
+    ((Table.find reserve_pool s).curr_hp > 0)
+
+(*Sets the active steammon from reserve_pool if the active steammon*)
+(*faints or has not been chosen yet. If all reserve steammons are 
+ *fainted, a game_result is set.*)
+let battle_starter g c ac : game_result option =
+  let reserve_pool = GameState.get_reserve_pool g c in
+  match ac with
+  | SelectStarter s when (valid_steammon s reserve_pool) ->
+      let mon = Table.find reserve_pool s in
+      GameState.swap_active_steammon g c mon;
+      None
+  | _ -> 
+      match (arbitrary_starter g c) with
+      | (None, None) -> failwith "Starter invariant failure"
+      | (None, res) -> res
+      | (_, None) -> None
+      | (_, _) -> failwith "Starter invariant failure 2"
+
+let battle_phase g ra ba = 
+  match (GameState.get_active_mon g Red) with
+  | _ -> (None, None, None)
 
 (* If an invalid message or a message is missing, this 
  * method performs the default expected action and returns a 
@@ -48,7 +101,9 @@ let default_action (g:game) (c:color) : action =
 
 (* Given the two actions, completes the action of the player 
  * running first and then completes the second. Returns a tuple
- * of command for red palyer, command for blue player and a result *)
+ * of command for red palyer, command for blue player and a result 
+ * Each function's output called by the action handler should match the 
+ * output of the action_handler *)
 let action_handler (g:game) (ra:action) (ba: action) : 
   command option * command option * game_result option = 
   let current_phase = GameState.get_phase g in
@@ -100,10 +155,10 @@ let handle_step (g:game) (ra:command) (ba:command) : game_output =
       GameState.set_exp g Blue (PickSteammon "");
       let r_pick_req = 
         Request (PickRequest (Red, (game_datafication g), 
-        (GameState.get_move_list g), (GameState.get_steammon_list g))) in
+        (GameState.get_move_list g), (GameState.get_steammon_list g Red))) in
       let b_pick_req = 
         Request (PickRequest (Blue, (game_datafication g), 
-        (GameState.get_move_list g), (GameState.get_steammon_list g))) in
+        (GameState.get_move_list g), (GameState.get_steammon_list g Red))) in
       (None, (game_datafication g), Some r_pick_req, Some b_pick_req)
 
   (* Both players respond with an action *)
@@ -134,15 +189,11 @@ let handle_step (g:game) (ra:command) (ba:command) : game_output =
       (result, (game_datafication g), red_request, blue_request)
 
 let init_game () : game * request * request * move list * steammon list =
-  (* Loading moves list and Steammon list *)
-  Initialization.init_pool "moves.csv" "steammon.csv";
-  let mvs = hash_to_list Initialization.move_table in
-  let mons = hash_to_list Initialization.mon_table in
-
   (* Creating a blank state for the beginning of the game *)
   let init_state = !game_instance in
 
-  (* Setting the move list and the Steammon list for the game *)
-  GameState.set_move_list init_state mvs;
-  GameState.set_steammon_list init_state mons;
+  (* Loading moves list and Steammon list *)
+  let mvs = GameState.get_move_list init_state in
+  let mons = GameState.get_base_mons init_state in
+
   (init_state, TeamNameRequest, TeamNameRequest, mvs, mons)
--- release/game/state.ml
+++ release/game/state.ml
@@ -1,5 +1,6 @@
 open Definitions
 open Constants
+open Util
 
 module GameState = struct
   (* State is represented as a record with mutable fields
@@ -14,22 +15,21 @@ module GameState = struct
   type active_steammon = {
     mutable mon : steammon;
     mutable can_use_moves : bool;
-    mutable effective_speed : int;
   }
 
   type player = {
     mutable inv : inventory;
     mutable active_mon : active_steammon option;
     mutable expected_action : action;
-    mutable steammons : steammon list;
+    mutable steammons : steammon Table.t option;
     mutable credits : int;
   }
 
   type state = { 
     mutable red_name : string option;
     mutable blue_name : string option;
-    mutable mvs : move list;
-    mutable mons : steammon list;
+    mutable mvs : move Table.t;
+    mutable base_mons : steammon Table.t;
     mutable red : player;
     mutable blue : player;
     mutable first : color;
@@ -40,7 +40,7 @@ module GameState = struct
     inv = []; 
     active_mon = None;
     expected_action = SendTeamName "Red";
-    steammons = [];
+    steammons = None;
     credits = cSTEAMMON_CREDITS; 
   }
 
@@ -48,28 +48,49 @@ module GameState = struct
     inv = []; 
     active_mon = None;
     expected_action = SendTeamName "Blue";
-    steammons = [];
+    steammons = None;
     credits = cSTEAMMON_CREDITS; 
   }
 
-  let initial_state () = {
+  let initial_state () =
+    Initialization.init_pool "moves.csv" "steammon.csv";
+    {
     red_name = None;
     blue_name = None;
-    mvs = [];
-    mons = [];
+    mvs = Initialization.move_table;
+    base_mons = Initialization.mon_table;
     red = init_red ();
     blue = init_blue ();
     first = Red;
     phase = TeamName;
-  }
+    }
 
   let get_name s c = 
     match c with 
     | Red -> s.red_name 
     | Blue -> s.blue_name
-  let get_move_list s = s.mvs
+  let get_move_list s = hash_to_list s.mvs
+  let get_base_mons s = hash_to_list s.base_mons
 
-  let get_steammon_list s = s.mons
+  (* Given a state and a player colour, returns all the
+   * steammons the player possesses *)
+  let get_steammon_list s c : steammon list = 
+    let player = match c with
+    | Red -> s.red
+    | Blue -> s.blue in
+    match (player.steammons, player.active_mon)  with
+    | (None, None) -> [] 
+    | (None, Some m) -> [m.mon]
+    | (Some ms, None) -> hash_to_list ms
+    | (Some ms, Some m) -> (m.mon)::(hash_to_list ms)
+
+  let get_reserve_pool s c : steammon Table.t =
+    let player = match c with
+    | Red -> s.red
+    | Blue -> s.blue in
+    match player.steammons with
+    | Some tbl -> tbl
+    | None -> failwith "Reserve pool not initialized"
 
   (* ************************* *)
   (* might not need 
@@ -83,14 +104,6 @@ module GameState = struct
     | Some mon -> steam.species = mon.species
    *)
 
-  let get_player_steammons s c  = 
-    match c with
-    | Red -> (match s.red.active_mon with
-	     | None -> s.red.steammons
-	     | Some x -> x.mon::s.red.steammons)
-    | Blue -> match s.blue.active_mon with
-	     | None -> s.blue.steammons
-	     | Some x -> x.mon::s.blue.steammons
   let get_inv s c = 
     match c with
     | Red -> s.red.inv
@@ -105,26 +118,26 @@ module GameState = struct
     | Blue -> s.blue.credits
   let get_phase s = 
     s.phase
-
-  let get_eff_speed s c = 
-    match c with
-    | Red -> (match s.red.active_mon with
-	     | None -> 0
-	     | Some x -> x.effective_speed)
-    | Blue -> match s.blue.active_mon with
-	     | None -> 0
-	     | Some x -> x.effective_speed
+  (*let get_eff_speed s c = *)
+    (*match c with*)
+    (*| Red -> (match s.red.active_mon with*)
+			 (*| None -> 0*)
+			 (*| Some x -> x.effective_speed)*)
+    (*| Blue -> match s.blue.active_mon with*)
+			 (*| None -> 0*)
+			 (*| Some x -> x.effective_speed*)
+  let get_active_mon s c =
+    let player = match c with
+    | Red -> s.red
+    | Blue -> s.blue in
+    match player.active_mon with
+    | None -> None
+    | Some active_mon -> Some (active_mon.mon)
 
   let set_name s c name = 
     match c with
     | Red -> s.red_name <- (Some name)
     | Blue -> s.blue_name <- (Some name)
-  let set_move_list s mv_list = s.mvs <- mv_list
-  let set_steammon_list s mon_list = s.mons <- mon_list
-  let set_player_steammons s c l = 
-    match c with
-    | Red -> s.red.steammons <- l
-    | Blue -> s.blue.steammons <- l
   let set_inv s c inv = 
     match c with
     | Red -> s.red.inv <- inv
@@ -147,14 +160,55 @@ module GameState = struct
     | Blue -> match s.blue.active_mon with
 	     | None -> raise NO_ACTIVE_STEAMMON
 	     | Some x -> x.can_use_moves <- boolean
-  let set_eff_speed s c speed = 
-    match c with
-    | Red -> (match s.red.active_mon with
-	     | None -> raise NO_ACTIVE_STEAMMON
-	     | Some x -> x.effective_speed <- speed)
-    | Blue -> match s.blue.active_mon with
-	     | None -> raise NO_ACTIVE_STEAMMON
-	     | Some x -> x.effective_speed <- speed
+  (*let set_eff_speed s c speed = *)
+    (*match c with*)
+    (*| Red -> (match s.red.active_mon with*)
+			 (*| None -> raise NO_ACTIVE_STEAMMON*)
+			 (*| Some x -> x.effective_speed <- speed)*)
+    (*| Blue -> match s.blue.active_mon with*)
+			 (*| None -> raise NO_ACTIVE_STEAMMON*)
+			 (*| Some x -> x.effective_speed <- speed*)
+  let set_active_mon s c m =
+    (* Update ALL FIELDS OF THE ACTIVE STEAMMON *)
+    let player = match c with
+    | Red -> s.red
+    | Blue -> s.blue in
+    match player.active_mon with
+    | Some active_mon -> active_mon.mon <- m
+    | None -> player.active_mon <- (Some ({mon = m; can_use_moves = true;}))
+
+  let add_reserve_steammon s c m = 
+    let player = match c with
+    | Red -> s.red 
+    | Blue -> s.blue in
+    match player.steammons with
+    | Some tbl -> Table.add tbl m.species m 
+    | None -> 
+        let new_reserve_pool = Table.create 1 in
+        Table.add new_reserve_pool m.species m;
+        player.steammons <- (Some new_reserve_pool)
+
+  let remove_reserve_steammon s c m = 
+    let player = match c with
+    | Red -> s.red 
+    | Blue -> s.blue in
+    match player.steammons with
+    | Some tbl -> Table.remove tbl m.species 
+    | None -> failwith "Reserve pool empty."
+
+  let swap_active_steammon s c m = 
+    let player = match c with
+    | Red -> s.red
+    | Blue -> s.blue in
+    match player.active_mon with
+    | None -> 
+        remove_reserve_steammon s c m;
+        player.active_mon <- (Some ({mon = m; can_use_moves = true;}))
+    | Some active_mon -> 
+        remove_reserve_steammon s c m;
+        add_reserve_steammon s c active_mon.mon;
+        active_mon.mon <- m
+
 
   (* Comparing the constructors for the actions to determine 
    * whether the expected action matches the responded action *)
