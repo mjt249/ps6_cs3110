070da613569e627e041e948e2abdaaaf0bf56b1a
sheroze1123 <mss385@cornell.edu>
2014-12-04 20:16:53 -0500

    Working self attack graphics update

--- release/game/game.ml
+++ release/game/game.ml
@@ -781,11 +781,6 @@ let use_move g c move_str : game_result option =
           else
             if m.target = User then
               (let mon = reduce_pp g c i in
-              print_string "Self move ";
-              print_int i;
-              print_string " Steammon ";
-              print_string mon.species;
-              print_endline "";
               let opp_mon = mon in
               let (mult, eff) = calc_multiplier mon opp_mon m in
               let damage = 
@@ -794,18 +789,13 @@ let use_move g c move_str : game_result option =
                 else if is_special m.element then 
                   calculate_damage mon.spl_attack opp_mon.spl_defense m.power mult
                 else calculate_damage mon.attack opp_mon.defense m.power mult in
-              print_string "Move: ";
-              print_string m.name;
-              print_string " Damage: ";
-              print_int damage;
-              print_endline "";
               let (targ, targ_color) = get_target mon opp_mon m c in
               do_damage g targ damage targ_color;
               let effect_list = traverse_effects g m mon opp_mon c damage in
               let move_update = {
                 name = m.name;
                 element = m.element;
-                from = opp_color targ_color;
+                from = targ_color;
                 toward = targ_color;
                 damage = damage;
                 hit = Hit;
@@ -824,24 +814,12 @@ let use_move g c move_str : game_result option =
                   | None -> failwith "Opponent has no Steammon"
                   | Some mon -> mon in
                     let (mult, eff) = calc_multiplier mon opp_mon m in
-                    assert (mult >= 0.);
-		    let damage = 
+                    let damage = 
                       if m.power = 0 then 
                         0 (*non damaging *)
                       else if is_special m.element then 
                         calculate_damage mon.spl_attack opp_mon.spl_defense m.power mult
                       else calculate_damage mon.attack opp_mon.defense m.power mult in
-                    print_string "Move: ";
-                    print_string m.name;
-                    print_string " Damage: ";
-                    print_int damage;
-                    print_endline "";
-		    assert (mon.spl_attack >= 0);
-		    assert (opp_mon.spl_defense >= 0);
-		    assert (mon.attack >= 0);
-		    assert (opp_mon.defense >= 0);
-		    assert (m.power >= 0);
-		    assert (damage >= 0);
                     let (targ, targ_color) = get_target mon opp_mon m c in
                     do_damage g targ damage targ_color;
                     let effect_list = traverse_effects g m mon opp_mon c damage in
